5a8f6ba621c95615ecab1311e1efca18e3a523ea
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-03-26 19:42:07 -0400

    datalist added

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-const form = document.querySelector('.form')
-document.createElement('form');
+// const form = document.querySelector('.form')
+// document.createElement('form');
 
-form.addEventListener('form', event => {
-    event.preventDefault();
-})
+// form.addEventListener('click', event => {
+//     event.preventDefault();
+// })
 
-const button = document.querySelector('.button')
-document.createElement('button');
+// const submit = document.querySelector('.submit')
+// document.createElement('submit');
 
-button.addEventListener('button', event => {
-    event.preventDefault();
-})
+// submit.addEventListener('click', event => {
+//     event.preventDefault();
+    
+// })
+
+
+// fetch('https://reqres.in/api/users')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data:', error);
+//   });
+
+
+
+const body = document.body;
+async function getData() {
+  try {
+    const response = await fetch('https://reqres.in/api/users');
+    const data = await response.json();
+
+    console.log(`Email: ${data.data[0].email}, Name: ${data.data[0].first_name}`);
+    let text = document.createElement('p');
+    text.innerText = `Email: ${data.data[0].email}, Name: ${data.data[0].first_name}`;
+    body.appendChild(text);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getData();
